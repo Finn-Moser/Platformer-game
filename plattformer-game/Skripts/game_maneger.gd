167e51d3edb1ccf20a865c45a3ef1e4fbe2302fd
Finn Moser <finn.moser@outlook.com>
2026-07-10 15:16:06 +0200

    simple fixes

--- plattformer-game/Skripts/game_maneger.gd
+++ plattformer-game/Skripts/game_maneger.gd
@@ -1,69 +1,75 @@
 extends Node
 
-@export var hearts: Array[TextureRect]        
-@onready var label: Label = %Label           
-@onready var coin_points_plus: Area2D = $"../Coin points plus"
+@export var hearts: Array[TextureRect]
 
-
-
+@onready var label: Label = %Label
+@onready var coin_points_plus: Area2D = get_node_or_null("../Coin points plus")
 
 
 var lives: int = 3
 var points: int = 0
 var max_health: int = 3
+
 var checkpoint_position: Vector2 = Vector2.ZERO
 var has_checkpoint: bool = false
 
+var player: Node2D
 
 
-#  checkpoint
+func _ready() -> void:
+	update_hearts()
+
+
+# CHECKPOINT
 func set_checkpoint(pos: Vector2) -> void:
 	checkpoint_position = pos
 	has_checkpoint = true
-	print("Checkpoint saved:", pos)
+	print("Checkpoint saved: ", pos)
 
-var player: Node2D
 
-func register_player(p):
+
+func register_player(p: Node2D) -> void:
 	player = p
 
-func decrease_health():
+
+
+func decrease_health() -> void:
 	if lives <= 0:
 		return
 
 	lives -= 1
-	print("Lives left:", lives)
+	print("Lives left: ", lives)
 
-	if lives >= 0 and lives < hearts.size():
-		hearts[lives].visible = false
+	update_hearts()
 
-	if lives == 0:
+	if lives <= 0:
 		get_tree().reload_current_scene()
 
 
 
 func add_health(amount: int = 1) -> void:
 	lives = min(lives + amount, max_health)
-	print("Lives:", lives)
-	_update_hearts()
+
+	print("Lives: ", lives)
+
+	update_hearts()
 
 
 
-func _update_hearts() -> void:
+func update_hearts() -> void:
 	for i in range(hearts.size()):
 		hearts[i].visible = i < lives
 
 
-func add_point(amount: int = 1):
+func add_point(amount: int = 1) -> void:
 	points += amount
 	update_label()
 
 
-func remove_point(amount: int = 1):
+func remove_point(amount: int = 1) -> void:
 	points -= amount
 	update_label()
 
 
-
-func update_label():
+func update_label() -> void:
 	label.text = str(points)
