a3ea31d9dda8214e1ab66f0bedcceed525175c3d
Finn Moser <finn.moser@outlook.com>
2026-07-10 14:35:27 +0200

    Fixes

--- plattformer-game/Skripts/coin.gd
+++ plattformer-game/Skripts/coin.gd
@@ -1,8 +1,42 @@
 extends Area2D
 
+
 @onready var game_manager: Node = %GameManeger
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var pickup_sound: AudioStreamPlayer2D = $pickup
 
-func _on_body_entered(_body: Node2D) -> void:
+
+var collected: bool = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if collected:
+		return
+
+	
+	if body.name != "Player":
+		return
+
+	collected = true
+
+	
+	monitoring = false
+
+	
+	game_manager.add_point(1)
+
+	
+	pickup_sound.play()
+
+	
 	animation_player.play("Pickup")
-	game_manager.add_point()
+
+	
+	await animation_player.animation_finished
+
+	
+	if pickup_sound.playing:
+		await pickup_sound.finished
+
+	
+	queue_free()
